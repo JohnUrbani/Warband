79b73170e83a52250132c160c3c63fca77ad456a
John Urbani <35612888+JohnUrbani@users.noreply.github.com>
2018-11-29 09:04:23 -0500

    Documenation Added
    Probably should've been doing this since the beginning.

--- main.py
+++ main.py
@@ -1,41 +1,58 @@
+# Warband by John Urbani 11/29/2018
 # Text adventure style game where the player leads an army through an invasion
-# in which they have to take over territory and recruit as they go. Plays similar
+# in which they have to take over a castle and recruit as they go. Plays similar
 # to Oregon Trail but you can get new members and there aren't random events, every
-# run will have the same options at the same areas
+# run will have the same options at the same areas but with different numbers as
+# each event that changes a stat is randomized other than buying with gold.
 import random
 SAVE_FILE = 'save.txt'
 
 
+# Start game menu with options, start the game either by loading the save file or
+# by starting a new game. Also have the option for exiting the game.
 def main():
+    # Repeat Bool, changes when given a working option such as new game or continue
+    # if a file with data exists.
     repeat = True
     while repeat:
+        # Give the player 3 options, new game, continue, or exit game
         choose_game = input('[N]ew Game?\n[C]ontinue?\n[E]xit\n').lower()
         while choose_game != 'n' and choose_game != 'c' and choose_game != 'e':
             choose_game = input('Choose a valid option\n').lower()
 
+        # New Game Option
         if choose_game == 'n':
             repeat = False
+            # Creates a generic save file with the default new game data
             new_save()
+            # Start the game and enter the game loop
             start()
+        # Continue Option
         elif choose_game == 'c':
             try:
+                # Open the save file
                 save = open(SAVE_FILE, 'r')
             except IOError:
+                # If there is no save file, ask the player to start a new game instead
                 print('No save file found, please choose [N] to create a new game')
             else:
+                # If the save file is blank, ask the player to start a new game instead
                 if save.readline() == '':
                     print('No save data found, please choose [N] to create a new game')
                     save.close()
                 else:
+                    # If the file is real and has data, start the game and enter the game loop
                     repeat = False
                     print('Save found')
                     save.close()
                     start()
+        # Exit Option
         elif choose_game == 'e':
+            # Exit the game
             exit()
 
 
-# Creates or updates the save file into its blank, default, state
+# Creates or updates the save file into its default, new game, state
 def new_save():
     save = open(SAVE_FILE, 'w')
     # File saved with these stats, split by commas
@@ -55,28 +72,45 @@ def new_save():
     # Castle 0 (Lose 8-20 men, if survived you win)
     # Castle after camp 0 (Lose 5-12 men, if survived you win)
     save_data = ['10', '20', '20', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
+    # Write the data from the list into the file
     for val in save_data:
         save.write(val + ',')
     save.close()
 
 
+# Read the save file, clear the screen, then start the game loop
+# with the data from the save file put into a list
 def start():
     save = open(SAVE_FILE, 'r')
     save_data = save.readline().split(',')
     save.close()
-    for i in range(50):
-        print()
+    clear_screen()
     game_loop(save_data)
 
 
+# The main loop of the game, repeats for every level the player goes through
 def game_loop(save_data):
+    # While the player has men and food
     while int(save_data[0]) > 0 and int(save_data[2]) > 0:
+        # Display the stats for the player (Men, Gold, and Food)
         show_stats(save_data)
+        # Go into the next level and give the player options to choose from
         choices(save_data)
+        # Clear the screen
+        clear_screen()
+        # Check if the player would lose before the next level
         game_over_check(save_data)
+        # Save the current game data to the save file
         save_game(save_data)
 
 
+# Prints 50 times, clearing the screen
+def clear_screen():
+    for i in range(50):
+        print()
+
+
+# Given a list of game data, overwrite the save file with this data
 def save_game(save_data):
     save = open(SAVE_FILE, 'w')
     for val in save_data:
@@ -84,14 +118,18 @@ def save_game(save_data):
     save.close()
 
 
+# Display players stats
 def show_stats(save_data):
     men = save_data[0]
     gold = save_data[1]
     food_loss = str(int(men) // 2)
     food = save_data[2]
-    print('Men: ' + men + '/50     Gold: ' + gold + '     Food: ' + food + '(-' + food_loss + '/travel)\n')
+    # Print each stat including the amount of food lost between levels
+    print('Men: ' + men + '     Gold: ' + gold + '     Food: ' + food + '(-' + food_loss + '/travel)\n')
 
 
+# Find which level the player is on and calculate food lost between levels
+# Once the level is found, call the level's function
 def choices(save_data):
     if save_data[3] == '0':
         level1(save_data)
@@ -113,13 +151,35 @@ def choices(save_data):
         level9(save_data)
     elif save_data[12] == '0':
         level10(save_data)
+    # If every level is completed tell the player they won
     else:
         print()
         print('Congratulations you won!')
+        # Give the player the option to either start a new game or exit the game
         play_or_exit()
 
+    # Calculate food lost between levels and lower the food count
     food_loss = str(int(save_data[0]) // 2)
     save_data[2] = str(int(save_data[2]) - int(food_loss))
+
+
+# Each level follows a standard formula, print 3 to 4 lines to tell the player about
+# the area or situation, then give the player a few options to choose from. The option
+# to continue onward will advance the player to the next level, the other options tell
+# the player what they are doing and change the player's stats accordingly, such as
+# buying food at a market increasing food while lowering gold.
+
+# When the player moves on to a new level, the save data associated with the
+# previous level is changed from 0 to 1 to indicate that it was completed.
+
+# Most actions the player can take can be done multiple times, however on the few that
+# can only be done once there is a Bool associated with the action that's checked in
+# the if statement, once its completed for the first time the Bool is changed and the
+# player can't take the action again, being given some text to explain the situation
+# instead.
+
+# After each action that results in the loss of food or men the game_over_check()
+# function is called to check if the player's action would result in a game over.
 
 
 # Homeland
@@ -457,20 +517,27 @@ def level10(save_data):
     save_data[12] = 1
 
 
+# Checks if the current game data would result in a game over,
+# this occurs when men or food go to 0 or lower
 def game_over_check(save_data):
+    # Check if men is greater than 0
     if int(save_data[0]) <= 0:
         print('Losing your last men, you find yourself alone.')
         print('You cannot continue the invasion by yourself.')
         print('GAME OVER')
+        # Give the player the option to restart or exit the game
         play_or_exit()
 
+    # Check if food is greater than 0
     if int(save_data[2]) <= 0:
         print('With your food supplies dwindling, your men return home.')
         print('You cannot continue the invasion by yourself.')
         print('GAME OVER')
+        # Give the player the option to restart or exit the game
         play_or_exit()
 
 
+# Give the player the option to either start a new game or exit the game
 def play_or_exit():
     choice = (input('[P]lay Again?     [E]xit\n')).lower()
     while choice != 'p' and choice != 'e':
